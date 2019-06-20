b15ca6019f0681a0567facc553a7a3f9701310c5
smjoseph <smjoseph@princeton.edu>
2019-06-19 20:55:58 -0400

    fix

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "test",
+        "excerpt":"s = \"Python syntax highlighting\" print s  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/test/",
+        "teaser":null}]
