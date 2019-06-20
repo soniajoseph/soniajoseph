6c1557e8be87697abd37e774444d382c17581b57
smjoseph <smjoseph@princeton.edu>
2019-06-19 21:02:00 -0400

    edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "test",
-        "excerpt":"s = \"Python syntax highlighting\" print s  ","categories": [],
+        "title": "Test",
+        "excerpt":" ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/test/",
         "teaser":null}]
