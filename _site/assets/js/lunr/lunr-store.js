115a1be513576e320a9acd399be7a8d71d68ece1
smjoseph <smjoseph@princeton.edu>
2019-06-19 22:20:37 -0400

    try header on blog post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Test Post",
+        "title": null,
         "excerpt":"H2 Heading   s = \"Python syntax highlighting\" print s  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/test/",
+        "tags": null,
+        "url": "http://localhost:4000/2019-06-19-test/",
         "teaser":null}]
