f744664d2fd42d1648dbdfd37284fb670c3acb6d
smjoseph <smjoseph@princeton.edu>
2019-07-04 23:02:19 -0400

    new commits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Test Post",
         "excerpt":"H2 Heading   s = \"Python syntax highlighting\" print s  ","categories": [],
-        "tags": [],
+        "tags": ["computational neuroscience","neuroscience"],
         "url": "http://localhost:4000/test/",
         "teaser":null}]
