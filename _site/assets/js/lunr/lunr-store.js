8aa1235dd3b4029fd3cc0158e5837ca42fc7e427
smjoseph <smjoseph@princeton.edu>
2019-07-09 09:36:33 -0400

    fix teaser

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
+        "title": "Paper List for Neural Dynamics",
+        "excerpt":"How does higher-order behavior emerge from billions of neurons firing? The papers below explore neural dynamics in theoretical and experimental contexts, with a focus on interpreting higher-level behaviors from multiple single-neuron recordings. This list is originally from the Dynamics in Cognition course taught by David Tank at Princeton. Basic Concepts...","categories": ["computational neuroscience"],
+        "tags": ["computational neuroscience","neuroscience","literature review","neural dynamics","deep learning"],
+        "url": "http://localhost:4000/computational%20neuroscience/dynamics/",
+        "teaser":"http://localhost:4000/assets/images/posts/starlings2.jpg"},{
         "title": "Neural Dynamics: A Primer (Hopfield Networks)",
         "excerpt":"How does higher-order behavior emerge from billions of neurons firing? This post is a basic introduction to thinking about the brain in the context of dynamical systems, which I have found to be far more useful than more phrenology-like paradigms. I tried to keep this introduction as simple and clear...","categories": ["computational neuroscience"],
         "tags": ["computational neuroscience","neuroscience","machine learning","neural dynamics"],
         "url": "http://localhost:4000/computational%20neuroscience/neural-dynamics-primer/",
-        "teaser":"http://localhost:4000/assets/images/starlings2.jpg"},{
-        "title": null,
-        "excerpt":"How does higher-order behavior emerge from billions of neurons firing? The papers below explore neural dynamics in theoretical and experimental contexts, with a focus on interpreting higher-level behaviors from multiple single-neuron recordings. This list is originally from the Dynamics in Cognition course taught by David Tank at Princeton. Basic Concepts...","categories": [],
-        "tags": null,
-        "url": "http://localhost:4000/2019-06-19-dynamics/",
-        "teaser":null}]
+        "teaser":"http://localhost:4000/assets/images/starlings2.jpg"}]
