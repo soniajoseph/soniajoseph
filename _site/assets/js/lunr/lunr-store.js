42dfb98ce6d119cd5ba818ccdcf4f2880a7ee984
smjoseph <smjoseph@princeton.edu>
2019-07-08 23:52:31 -0400

    edits

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "Paper List for Neural Dynamics",
-        "excerpt":"How does higher-order behavior emerge from billions of neurons firing? The papers below explore neural dynamics in theoretical and experimental contexts, with a focus on interpreting higher-level behaviors from multiple single-neuron recordings. This list is originally from the Dynamics in Cognition course taught by David Tank at Princeton. Basic Concepts...","categories": ["computational neuroscience"],
+        "excerpt":"How does higher-order behavior emerge from billions of neurons firing? The papers below explore neural dynamics in theoretical and experimental contexts, with a focus on interpreting higher-level behaviors from multiple single-neuron recordings. This list is originally from the Dynamics in Cognition course taught by David Tank at Princeton. Basic Concepts...","categories": [],
         "tags": ["computational neuroscience","neuroscience","literature review","neural dynamics","deep learning"],
-        "url": "http://localhost:4000/computational%20neuroscience/dynamics/",
+        "url": "http://localhost:4000/dynamics/",
         "teaser":null},{
         "title": "Neural Dynamics: A Primer (Hopfield Networks)",
-        "excerpt":"How does higher-order behavior emerge from billions of neurons firing? This post is a basic introduction to thinking about the brain in the context of dynamical systems, which I have found to be far more useful than more phrenology-like paradigms. I tried to keep this introduction as simple and clear...","categories": ["computational neuroscience"],
+        "excerpt":"For a list of seminal papers in neural dynamics, go here. 1. Emergent Behavior from Simple Parts Physical systems made out of a large number of simple elements give rise to collective phenomena. For example, flying starlings: Each starling follows simple rules: coordinating with seven neighbors, staying near a fixed...","categories": [],
         "tags": ["computational neuroscience","neuroscience","machine learning","neural dynamics"],
-        "url": "http://localhost:4000/computational%20neuroscience/neural-dynamics-primer/",
+        "url": "http://localhost:4000/neural-dynamics-primer/",
         "teaser":null}]
